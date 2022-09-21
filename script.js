8e8712631ba68f110f3cde1911bc9cff4f15f658
linuxlite <eze.rey92@gmail.com>
2022-09-21 17:33:16 -0300

    primer canvas

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 const mouse = { x: undefined, y: undefined };
 const particlesArray = [];
+const particlesNumber = 2;
+let hue = 0;
 
 canvas.addEventListener("mousemove", ({ x, y }) => {
 	mouse.x = x;
 	mouse.y = y;
 
-	for (let index = 0; index < 10; index++) {
+	for (let index = 0; index < particlesNumber; index++) {
 		particlesArray.push(new Particles());
 	}
 });
@@ -18,7 +20,7 @@ class Particles {
 	constructor() {
 		this.x = mouse.x;
 		this.y = mouse.y;
-
+		this.color = ` hsl(${hue}, 100%, 50%)`;
 		this.size = Math.random() * 5 + 1;
 		this.speedX = Math.random() * 5 - 1.5;
 		this.speedY = Math.random() * 5 - 1.5;
@@ -30,7 +32,7 @@ class Particles {
 		if (this.size > 0.2) this.size -= 0.1;
 	}
 	draw() {
-		ctx.fillStyle = "white";
+		ctx.fillStyle = this.color;
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
 		ctx.fill();
@@ -43,6 +45,21 @@ function handdleParticles() {
 		particle.draw();
 		particle.update();
 
+		for (let j = i; j < particlesArray.length; j++) {
+			const particleJ = particlesArray[j];
+			const dx = particle.x - particleJ.x;
+			const dy = particle.y - particleJ.y;
+			const distance = Math.sqrt(dx * dx + dy * dy);
+			if (distance < 200) {
+				ctx.beginPath();
+				ctx.strokeStyle = particle.color;
+				ctx.lineWidth = particle.size / 10;
+				ctx.moveTo(particle.x, particle.y);
+				ctx.lineTo(particleJ.x, particleJ.y);
+				ctx.stroke();
+			}
+		}
+
 		if (particle.size <= 0.3) {
 			particlesArray.splice(i, 1);
 		}
@@ -51,7 +68,11 @@ function handdleParticles() {
 
 function animate() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	// ctx.fillStyle = "rgba(0,0,0,0.1)";
+	// ctx.fillRect(0, 0, canvas.width, canvas.height);
+	console.log(particlesArray);
 	handdleParticles();
+	hue += 2;
 	requestAnimationFrame(animate);
 }
 
